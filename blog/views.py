12d2866d9b11c2b8aabfb4c9d2a6660f2a632d94
paola <yipamulo@hotmail.com>
2017-02-12 18:40:57 -0500

    detail

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,6 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-	post = get_object_or_404(post, pk=pk)
-	return render(request, 'blog/post_detail.html', {'post': post})
+	p = get_object_or_404(post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': p})
 	
