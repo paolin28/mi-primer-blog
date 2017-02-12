12b285884768421459d2aea8f3688a3701be21da
paola <yipamulo@hotmail.com>
2017-02-12 18:29:54 -0500

    template html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
 
 
@@ -9,7 +9,7 @@ def post_list(request):
 	posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
-	def post_detail(request, pk):
-		post = get_object_or_404(post, pk=pk)
-		return render(request, 'blog/post_detail.html', {'post': post})
+def post_detail(request, pk):
+	post = get_object_or_404(post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
 	
