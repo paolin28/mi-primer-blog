facb96fa88a0cc813fbe7bae093994b5284e0b4d
paola <yipamulo@hotmail.com>
2017-02-12 17:00:22 -0500

    plantillas y html

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from django.utils import timezone
 
 def post_list(request):
 	posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {})
+	return render(request, 'blog/post_list.html', {'posts': posts})
 	
